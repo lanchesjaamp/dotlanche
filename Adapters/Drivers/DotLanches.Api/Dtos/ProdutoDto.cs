07dd2040c2876ea55868ed0d035c36962f587dad
Andre Torres <atcorrea17@gmail.com>
2024-05-15 19:27:56 -0300

    adding produto controller

---
+++ Adapters/Drivers/DotLanches.Api/Dtos/ProdutoDto.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+namespace DotLanches.Api.Dtos
+{
+    public class ProdutoDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CategoriaId { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
