f78ee582ad1a9aabccef25592dcb96542401ea31
Andre Torres <atcorrea17@gmail.com>
2024-05-15 19:26:51 -0300

    configure database for produto and categoria

---
+++ Adapters/Drivens/DotLanches.Infra/Data/DotLanchesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DotLanches.Domain.Entities;
+using DotLanches.Infra.ModelConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotLanches.Infra.Data
+{
+    internal class DotLanchesDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DotLanchesDbContext(DbContextOptions<DotLanchesDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            new CategoriaModelConfiguration().Configure(modelBuilder.Entity<Categoria>());
+            new ProdutoModelConfiguration().Configure(modelBuilder.Entity<Produto>());
+        }
+    }
+}
---
+++ Adapters/Drivens/DotLanches.Infra/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace DotLanches.Infra.Exceptions
+{
+    internal class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException() : base("entity not found!")
+        {
+        }
+    }
+}
