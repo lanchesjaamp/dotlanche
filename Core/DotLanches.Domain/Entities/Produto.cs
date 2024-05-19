90e1921baf07e5cc5a6dda76bf2e9450cb39165e
Andre Torres <atcorrea17@gmail.com>
2024-05-18 21:46:02 -0300

    chore: add produto validation

--- Core/DotLanches.Domain/Entities/Produto.cs
+++ Core/DotLanches.Domain/Entities/Produto.cs
@@ -1,12 +1,42 @@
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+using DotLanches.Domain.Exceptions;
 
 namespace DotLanches.Domain.Entities;
 
 public class Produto
 {
-    public int Id { get; set; }
+    public int Id { get; private set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
     public Categoria Categoria { get; set; }
+
+    private Produto() { }
+
+    public Produto(int id,
+                   string name,
+                   string description,
+                   decimal price,
+                   Categoria categoria)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Price = price;
+        Categoria = categoria;
+
+        ValidateEntity();
+    }
+
+    private void ValidateEntity()
+    {
+        if (string.IsNullOrEmpty(Name))
+            throw new DomainValidationException(nameof(Name));
+        if (string.IsNullOrEmpty(Description))
+            throw new DomainValidationException(nameof(Description));
+        if (Price <= 0)
+            throw new DomainValidationException(nameof(Price));
+        if (Categoria is null)
+            throw new DomainValidationException(nameof(Categoria));
+    }
 }
