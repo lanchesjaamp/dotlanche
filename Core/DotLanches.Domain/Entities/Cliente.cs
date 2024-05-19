9cea154bdf48a8cbd8b1143770a0aca930cf1b44
Angelo Costa Neto <angelo_costa_neto@outlook.com>
2024-05-19 13:33:11 -0300

    Update cliente dto, cliente entity and cliente mapper

--- Core/DotLanches.Domain/Entities/Cliente.cs
+++ Core/DotLanches.Domain/Entities/Cliente.cs
@@ -1,8 +1,30 @@
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+using DotLanches.Domain.Exceptions;
+
 namespace DotLanches.Domain.Entities;
 
 public class Cliente
 {
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public required string Cpf { get; set; }
+    public int Id { get; private set; }
+    public string Name { get; set; }
+    public string Cpf { get; set; }
+    
+    private Cliente() {}
+
+    public Cliente(int id, string name, string cpf)
+    {
+        Id = id;
+        Name = name;
+        Cpf = cpf;
+        
+        ValidateEntity();
+    }
+    
+    private void ValidateEntity()
+    {
+        if (string.IsNullOrEmpty(Name))
+            throw new DomainValidationException(nameof(Name));
+        if (string.IsNullOrEmpty(Cpf))
+            throw new DomainValidationException(nameof(Cpf));
+    }
 }
