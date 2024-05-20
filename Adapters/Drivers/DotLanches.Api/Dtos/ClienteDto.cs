20fa0d33ea2d501f21c12813823678dc457021b1
Angelo Costa Neto <angelo_costa_neto@outlook.com>
2024-05-20 19:53:34 -0300

    Add cpf validation and update migration

--- Adapters/Drivers/DotLanches.Api/Dtos/ClienteDto.cs
+++ Adapters/Drivers/DotLanches.Api/Dtos/ClienteDto.cs
@@ -10,5 +10,7 @@ namespace DotLanches.Api.Dtos
         public string Name { get; set; }
         [Required]
         public string Cpf { get; set; }
+        [Required]
+        public string Email { get; set; }
     }
 }
--- Adapters/Drivers/DotLanches.Api/Mappers/ClienteMapper.cs
+++ Adapters/Drivers/DotLanches.Api/Mappers/ClienteMapper.cs
@@ -7,7 +7,7 @@ namespace DotLanches.Api.Mappers
     {
         public static Cliente ToDomainModel(this ClienteDto clienteDto, int id = 0)
         {
-            var domainModel = new Cliente(id, clienteDto.Name, clienteDto.Cpf);
+            var domainModel = new Cliente(id, clienteDto.Name, clienteDto.Cpf, clienteDto.Email);
 
             return domainModel;
         }
