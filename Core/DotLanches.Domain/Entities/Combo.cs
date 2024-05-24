e4630ba75b2071da348c944c49cf54ec482dd91b
pedrofbaltar <pfbaltariead@gmail.com>
2024-05-24 19:36:39 -0300

    Updated entities

--- Core/DotLanches.Domain/Entities/Combo.cs
+++ Core/DotLanches.Domain/Entities/Combo.cs
@@ -1,4 +1,5 @@
-using System.Text.Json.Serialization;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+using DotLanches.Domain.Exceptions;
 
 namespace DotLanches.Domain.Entities;
 
@@ -6,24 +7,49 @@ public class Combo
 {
     public int Id { get; set; }
     public int PedidoId { get; set; }
-    public int? LancheId { get; set; }
-    public string? LancheName { get; set; }
-    public int? AcompanhamentoId { get; set; }
-    public string? AcompanhamentoName { get; set; }
-    public int? BebidaId { get; set; }
-    public string? BebidaName { get; set; }
-    public int? SobremesaId { get; set; }
-    public string? SobremesaName { get; set; }
+    public Produto? Lanche { get; set; }
+    public Produto? Acompanhamento { get; set; }
+    public Produto? Bebida { get; set; }
+    public Produto? Sobremesa { get; set; }
     public decimal Price { get; set; }
 
-    [JsonIgnore]
-    public Pedido? Pedido { get; set; }
-    [JsonIgnore]
-    public Produto? Lanche { get; set; }
-    [JsonIgnore]
-    public Produto? Acompanhamento { get; set; }
-    [JsonIgnore]
-    public Produto? Bebida { get; set; }
-    [JsonIgnore]
-    public Produto? Sobremesa { get; set; }
+    private Combo() { }
+
+    public Combo(int id,
+                 int pedidoId,
+                 Produto lanche,
+                 Produto acompanhamento,
+                 Produto bebida,
+                 Produto sobremesa)
+    {
+        Id = id;
+        PedidoId = pedidoId;
+        Lanche = lanche;
+        Acompanhamento = acompanhamento;
+        Bebida = bebida;
+        Sobremesa = sobremesa;
+
+        ValidateEntity();
+    }
+
+    private void ValidateEntity()
+    {
+        if (Lanche?.Id == 0 && Acompanhamento?.Id == 0 && Bebida?.Id == 0 && Sobremesa?.Id == 0)
+            throw new DomainValidationException(nameof(Produto));
+    }
+
+    public void CalculatePrice()
+    {
+        if (Lanche is not null)
+            Price += Lanche.Price;
+        if (Acompanhamento is not null)
+            Price += Acompanhamento.Price;
+        if (Bebida is not null)
+            Price += Bebida.Price;
+        if (Sobremesa is not null)
+            Price += Sobremesa.Price;
+
+        if (Price <= 0)
+            throw new DomainValidationException(nameof(Price));
+    }
 }
