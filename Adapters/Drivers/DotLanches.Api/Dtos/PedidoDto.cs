60a9a7ce65523df59c02c074d678b5e80a24e9ed
pedrofbaltar <pfbaltariead@gmail.com>
2024-05-24 19:28:25 -0300

    Changed from List to IEnumerable

--- Adapters/Drivers/DotLanches.Api/Dtos/PedidoDto.cs
+++ Adapters/Drivers/DotLanches.Api/Dtos/PedidoDto.cs
@@ -1,8 +1,12 @@
-﻿namespace DotLanches.Api.Dtos
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+using System.ComponentModel.DataAnnotations;
+
+namespace DotLanches.Api.Dtos
 {
     public class PedidoDto
     {
         public string? ClienteCPF { get; set; }
-        public List<ComboDto> Combos { get; set; }
+        [Required]
+        public IEnumerable<ComboDto> Combos { get; set; }
     }
 }
