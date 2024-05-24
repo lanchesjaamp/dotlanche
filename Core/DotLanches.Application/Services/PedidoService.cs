0f88e254e3feee134040000af41c05fb9d3ad7ed
pedrofbaltar <pfbaltariead@gmail.com>
2024-05-24 19:39:50 -0300

    Updated pedido repository and service

--- Core/DotLanches.Application/Services/PedidoService.cs
+++ Core/DotLanches.Application/Services/PedidoService.cs
@@ -1,48 +1,41 @@
-﻿using DotLanches.Domain.Entities;
+﻿#pragma warning disable CS8602 // Desreferência de uma referência possivelmente nula.
+using DotLanches.Domain.Entities;
 using DotLanches.Domain.Interfaces.Repositories;
 using DotLanches.Domain.Interfaces.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DotLanches.Application.Services
 {
     public class PedidoService : IPedidoService
     {
         private readonly IPedidoRepository _pedidoRepository;
-        private readonly IClienteService _clienteService;
+        private readonly IProdutoRepository _produtoRepository;
 
-        public PedidoService(IPedidoRepository pedidoRepository, IClienteService clienteService)
+        public PedidoService(IPedidoRepository pedidoRepository, IProdutoRepository produtoRepository)
         {
             _pedidoRepository = pedidoRepository;
-            _clienteService = clienteService;
+            _produtoRepository = produtoRepository;
         }
 
         public async Task Add(Pedido pedido)
         {
-            if (!string.IsNullOrEmpty(pedido.ClienteCPF))
-            {
-                var clientes = await _clienteService.GetAll();
-
-                if (clientes.FirstOrDefault(c => c.Cpf == pedido.ClienteCPF) == null)
-                {
-                    throw new ArgumentException("O CPF do cliente especificado no pedido não existe.");
-                }
-            }
-
             foreach (var combo in pedido.Combos)
             {
-                if (combo.LancheId is null && combo.AcompanhamentoId is null && combo.BebidaId is null && combo.SobremesaId is null)
-                {
-                    throw new ArgumentException("Pelo menos um item do combo deve ser preenchido.");
-                }
+                combo.Lanche = await _produtoRepository.GetById(combo.Lanche.Id);
+                combo.Acompanhamento = await _produtoRepository.GetById(combo.Acompanhamento.Id);
+                combo.Bebida = await _produtoRepository.GetById(combo.Bebida.Id);
+                combo.Sobremesa = await _produtoRepository.GetById(combo.Sobremesa.Id);
+
+                combo.CalculatePrice();
             }
+
+            pedido.CalculateTotalPrice();
 
             await _pedidoRepository.Add(pedido);
         }
 
-        public async Task<IEnumerable<Pedido>> GetAll() => await _pedidoRepository.GetAll();
+        public async Task<IEnumerable<Pedido>> GetAll()
+        {
+            return await _pedidoRepository.GetAll();
+        } 
     }
 }
