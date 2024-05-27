eec6023baaf166c657b393d33c09040a7cfa7da9
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-05-26 21:53:35 -0300

    Create dto for Pagamento return

---
+++ Adapters/Drivers/DotLanches.Api/Dtos/PagamentoDto.cs
@@ -0,0 +1,13 @@
+﻿using DotLanches.Domain.Entities;
+
+namespace DotLanches.Api.Dtos
+{
+    public class PagamentoDto
+    {
+        public bool? IsAccepted { get; set; }
+
+        public DateTime? RegisteredAt { get; set; }
+
+        public int QueueKey { get; set; } 
+    }
+}
