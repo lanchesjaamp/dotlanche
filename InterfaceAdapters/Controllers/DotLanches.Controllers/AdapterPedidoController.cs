225e986dcc0196071c65fcc76f6bf7aa144d6b34
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-07-11 22:29:13 -0300

    Controllers in adapters layer

---
+++ InterfaceAdapters/Controllers/DotLanches.Controllers/AdapterPedidoController.cs
@@ -0,0 +1,34 @@
+﻿using DotLanches.Application.UseCases;
+using DotLanches.Domain.Entities;
+using DotLanches.Domain.Interfaces.Repositories;
+using DotLanches.Gateways;
+
+namespace DotLanches.Controllers
+{
+    public class AdapterPedidoController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public AdapterPedidoController(IProdutoRepository produtoRepository, IPedidoRepository pedidoRepository)
+        {
+            _produtoRepository = produtoRepository;
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public async Task Create(Pedido pedido)
+        {
+            var produtoGateway = new ProdutoGateway(_produtoRepository);
+            var pedidoGateway = new PedidoGateway(_pedidoRepository);
+            await PedidoUseCases.Add(pedido, produtoGateway, pedidoGateway);
+            return;
+        }
+
+        public async Task<IEnumerable<Pedido>> GetAll()
+        {
+            var pedidoGateway = new PedidoGateway(_pedidoRepository);
+            var pedidoList = await PedidoUseCases.GetAll(pedidoGateway);
+            return pedidoList;
+        }
+    }
+}
