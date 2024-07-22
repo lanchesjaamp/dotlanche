2e12d5cd6762d89c8b7a15729858567cc43240c0
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-07-21 23:18:36 -0300

    Add usecases for payment response, accepted or refused

--- Core/DotLanches.Application/UseCases/PagamentoUseCases.cs
+++ Core/DotLanches.Application/UseCases/PagamentoUseCases.cs
@@ -6,7 +6,7 @@ namespace DotLanches.Application.UseCases
 {
     public static class PagamentoUseCases
     {
-        public static async Task<Pagamento> ProcessPagamento(int idPedido, IPedidoGateway pedidoGateway, IPagamentoGateway pagamentoGateway, ICheckout checkout)
+        public static async Task<string> ProcessPagamento(int idPedido, IPedidoGateway pedidoGateway, IPagamentoGateway pagamentoGateway, ICheckout checkout)
         {
             var pedido = await pedidoGateway.GetById(idPedido) ??
                 throw new Exception("Payment processing error: Non existing pedido!");
@@ -15,16 +15,43 @@ namespace DotLanches.Application.UseCases
             await pagamentoGateway.Add(pagamento);
 
             //Fake Checkout for current version
-            var paymentSucceded = checkout.ProcessPayment(pagamento);
+            var qrCode = checkout.ProcessPayment(pagamento);
 
-            if (paymentSucceded)
-            {
-                pagamento.ConfirmPayment();
-                pedido.ReceivePagamento();
+            return qrCode;
+        }
 
-                await pedidoGateway.Update(pedido);
-                pagamento = await pagamentoGateway.Update(pagamento);
-            }
+        public static async Task<Pagamento> AcceptedPagamento(int idPedido, IPedidoGateway pedidoGateway, IPagamentoGateway pagamentoGateway)
+        {
+            var pedido = await pedidoGateway.GetById(idPedido) ??
+                throw new Exception("Payment processing error: Non existing pedido!");
+
+            var pagamento = new Pagamento(pedido.Id);
+
+            await pagamentoGateway.Add(pagamento);
+
+            pagamento.ConfirmPayment();
+
+            pedido.ReceivePagamento();
+
+            await pedidoGateway.Update(pedido);
+
+            pagamento = await pagamentoGateway.Update(pagamento);
+
+            return pagamento;
+        }
+
+        public static async Task<Pagamento> RefusedPagamento(int idPedido, IPedidoGateway pedidoGateway, IPagamentoGateway pagamentoGateway)
+        {
+            var pedido = await pedidoGateway.GetById(idPedido) ??
+                throw new Exception("Payment processing error: Non existing pedido!");
+
+            var pagamento = new Pagamento(pedido.Id);
+
+            await pagamentoGateway.Add(pagamento);
+
+            pagamento.RefusePayment();
+
+            pagamento = await pagamentoGateway.Update(pagamento);
 
             return pagamento;
         }
