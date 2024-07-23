84898ec958664c1b9647fd18ba9ef046e0313a18
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-07-22 23:18:41 -0300

    Routes for category retrieval

---
+++ Core/DotLanches.Domain/Interfaces/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using DotLanches.Domain.Entities;
+
+namespace DotLanches.Domain.Interfaces.Repositories;
+
+public interface ICategoriaRepository
+{
+    public Task<IEnumerable<Categoria>> GetAll();
+
+    public Task<Categoria?> GetById(int idCategoria);
+}
---
+++ FrameworksAndDrivers/Web/DotLanches.Api/Controllers/CategoriaController.cs
@@ -0,0 +1,44 @@
+﻿using DotLanches.Domain.Entities;
+using DotLanches.Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotLanches.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriaController : ControllerBase
+{
+    private readonly ICategoriaRepository _categoriaRepository;
+
+    public CategoriaController(ICategoriaRepository categoriaRepository)
+    {
+        _categoriaRepository = categoriaRepository;
+    }
+
+    /// <summary>
+    /// Busca todas as categorias.
+    /// </summary>
+    /// <returns>Lista de categorias.</returns>
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Categoria>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAll()
+    {
+        var categorias = await _categoriaRepository.GetAll();
+        return Ok(categorias);
+    }
+
+    /// <summary>
+    /// Busca categoria referente ao ID informado.
+    /// </summary>
+    /// <param name="idCategoria">ID da categoria a ser buscada.</param>
+    /// <returns>Especificação da categoria referente ao ID.</returns>
+    [HttpGet("{idCategoria}")]
+    [ProducesResponseType(typeof(Categoria), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetById([Required][FromRoute] int idCategoria)
+    {
+        var categoria = await _categoriaRepository.GetById(idCategoria);
+        if(categoria is null) return NoContent();
+        return Ok(categoria);
+    }
+}
