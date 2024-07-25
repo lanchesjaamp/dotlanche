24b4395d62fc9f3820e1f88bd6b8ffcfca190682
Mauro Tuyoshi Imamura Junior <mauro.imamura@outlook.com>
2024-07-25 20:16:13 -0300

    Produto CRUD in CA layers

---
+++ InterfaceAdapters/Controllers/DotLanches.Controllers/AdapterProdutoController.cs
@@ -0,0 +1,51 @@
+﻿using DotLanches.Application.UseCases;
+using DotLanches.Domain.Entities;
+using DotLanches.Domain.Interfaces.Repositories;
+using DotLanches.Gateways;
+using System.Net.Http.Headers;
+
+namespace DotLanches.Controllers;
+
+public class AdapterProdutoController
+{
+    private readonly IProdutoRepository _produtoRepository;
+
+    public AdapterProdutoController(IProdutoRepository produtoRepository)
+    {
+        _produtoRepository = produtoRepository;
+    }
+
+    public async Task AddProduto(Produto produto)
+    {
+        var gateway = new ProdutoGateway(_produtoRepository);
+        await ProdutoUseCases.RegisterNewProduto(produto, gateway);
+    }
+
+    public async Task<Produto> EditProduto(Produto produto)
+    {
+        var gateway = new ProdutoGateway(_produtoRepository);
+        var prod = await ProdutoUseCases.EditExistingProduto(produto, gateway);
+        return prod;
+    }
+
+    public async Task<Produto> DeleteProduto(int idProduto)
+    {
+        var gateway = new ProdutoGateway(_produtoRepository);
+        var prod = await ProdutoUseCases.RemoveProduto(idProduto, gateway);
+        return prod;
+    }
+
+    public async Task<IEnumerable<Produto>> GetByCategoria(int categoriaId)
+    {
+        var gateway = new ProdutoGateway(_produtoRepository);
+        var produtos = await ProdutoUseCases.ShowAllProdutosForGivenCategory(categoriaId, gateway);
+        return produtos;
+    }
+
+    public async Task<Produto> GetById(int idProduto)
+    {
+        var gateway = new ProdutoGateway(_produtoRepository);
+        var produto = await ProdutoUseCases.ShowSelectedProduto(idProduto, gateway);
+        return produto;
+    }
+}
